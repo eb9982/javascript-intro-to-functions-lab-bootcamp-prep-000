a9c915545957e9a700c34751b6034dea5e4d8389
eb9982 <lborsare1@gmail.com>
2017-09-08 01:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string){
-  console.log(string).toUpperCase
-  console.log().restore
+  console.log(string)
+  
 }
 function sayHiToGrandma(string) {
   var uppercase = "HELLO"
